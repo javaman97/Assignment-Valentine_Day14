795e5bc66776e937c2225203bb8e8fc205873067
Aman Nishad <amannishad0512@gmail.com>
2023-02-15 02:57:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/assignmentvalentineappday14/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.assignmentvalentineappday14
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.cardview.widget.CardView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val cardViewHistory:CardView=findViewById(R.id.cardview_history)
+        val cardViewMessage:CardView=findViewById(R.id.cardview_message)
+        cardViewHistory.setOnClickListener {
+            val intent=Intent(this,First_Card::class.java)
+            startActivity(intent)
+        }
+        cardViewMessage.setOnClickListener {
+            val intent=Intent(this,Second_Card::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/assignmentvalentineappday14/Second_Card.kt
@@ -0,0 +1,11 @@
+package com.example.assignmentvalentineappday14
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Second_Card : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_card)
+    }
+}
